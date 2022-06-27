3e66d7e60017190d2a8585762c0a6a584fdf367e
chuchosch <104324952+chuchosch@users.noreply.github.com>
2022-06-27 10:56:31 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// console.log("Bienvenido a su caminata");
+// let pasos = prompt("Cuantos pasos quiere hacer?");
+// let i = 0;
+// while (i < pasos) {
+//     i++;
+//     alert(`usted va ${i} pasos caminados`);
+//     if  (i%2 == 0){
+//       alert("Lleva un numero par de pasos")
+//     }
+//     else{
+//     alert("Lleva un numero impar de pasos");
+//     }
+// }
+console.log("Cantidad de entradas");
+let cantidad = prompt("Cuantas entradas queres?");
+if (i > 0) {
+  alert(`usted quiere ${i} entradas`)
+}
+else { 
+  alert("vuelva a ingresar una cantidad")
+}
